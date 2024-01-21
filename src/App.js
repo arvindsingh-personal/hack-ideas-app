0303f25aef81002da5a33ba65a766a3ac2df4e6a
arvindsingh-personal <arvindsingh61885@gmail.com>
2024-01-21 23:40:54 +0530

    installed Ant-D for styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { DatePicker } from "antd";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="">
+      <DatePicker />
+    </div>
+  );
 }
 
 export default App;
